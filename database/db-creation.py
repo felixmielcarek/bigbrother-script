a802d8366e7d95ecb1e366c86f3099ec9d551a08
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2024-05-15 20:37:55 +0200

    DB script update

--- database/db-creation.py
+++ database/db-creation.py
@@ -17,15 +17,7 @@ def executeDBQuery(query):
     conn.close()
 
 create_db_query = '''
-CREATE DATABASE bigbrother
-    WITH
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'French_France.1252'
-    LC_CTYPE = 'French_France.1252'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+CREATE DATABASE bigbrother;
 '''
 
 create_schema_query = '''
