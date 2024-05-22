e938e6f07b0e5cbdaeab1498efc6b125aaa6d63e
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2024-05-22 14:20:54 +0200

    Changing token recovery request type

--- callback-server/app.js
+++ callback-server/app.js
@@ -24,60 +24,12 @@ app.listen(port, () => { console.log(`Big brother is listening on port ${port}`)
 //#endregion
 
 //#region ACCESS TOKEN
-app.get('/settings/deactivate', async (req,res) => {
-  stepBeggining("Setting: deactivation");
-  
-  const code = req.query.code;
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token', method: 'post', json: true,
-    data: { code: code, redirect_uri: redirectUri, grant_type: 'authorization_code' },
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-    }
-  };
-  let userId;
-
-  try {
-    const response1 = await axios(authOptions);
-    const accessToken = response1.data.access_token;
-    const response2 = await axios.get(`https://api.spotify.com/v1/me`, { headers: { 'Authorization': 'Bearer ' + accessToken, } });
-    userId = response2.data.SpotifyId;
-  } catch (error) { console.log(`Error getting user Spotify id: ${error}`) }
-
-  const sqlQuery = `
-    DELETE FROM users
-    WHERE spotifyid = ?;
-  `;
-  
-  try {    
-    const pool = mariadb.createPool({
-      host: 'felixmielcarek-bigbrotherdb',
-      user: process.env.MARIADB_USER,
-      database: process.env.MARIADB_DATABASE,
-      password: process.env.MARIADB_PASSWORD,
-      connectionLimit: 5
-    });
-
-    const conn = await pool.getConnection();
-    
-    conn.query(sqlQuery, [userId], (err, res) => {
-      if (err) {
-        console.error('Error executing query', err);
-        return;
-      }
-      console.log('Data deleted successfully');
-      conn.end();
-    });
-  } catch (error) { console.log(`Error accessing database: ${error}`) }
-})
-
 app.get('/', async (req, res) => {
   stepBeggining("Activation");
   
   const code = req.query.code;
   const authOptions = {
-    url: 'https://accounts.spotify.com/api/token', method: 'post', json: true,
+    url: 'https://accounts.spotify.com/api/token', method: 'get', json: true,
     data: { code: code, redirect_uri: redirectUri, grant_type: 'authorization_code' },
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
@@ -123,6 +75,54 @@ app.get('/', async (req, res) => {
         return;
       }
       console.log('Data inserted/updated successfully');
+      conn.end();
+    });
+  } catch (error) { console.log(`Error accessing database: ${error}`) }
+});
+
+app.get('/settings/deactivate', async (req,res) => {
+  stepBeggining("Setting: deactivation");
+  
+  const code = req.query.code;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token', method: 'post', json: true,
+    data: { code: code, redirect_uri: redirectUri, grant_type: 'authorization_code' },
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+    }
+  };
+  let userId;
+
+  try {
+    const response1 = await axios(authOptions);
+    const accessToken = response1.data.access_token;
+    const response2 = await axios.get(`https://api.spotify.com/v1/me`, { headers: { 'Authorization': 'Bearer ' + accessToken, } });
+    userId = response2.data.SpotifyId;
+  } catch (error) { console.log(`Error getting user Spotify id: ${error}`) }
+
+  const sqlQuery = `
+    DELETE FROM users
+    WHERE spotifyid = ?;
+  `;
+  
+  try {    
+    const pool = mariadb.createPool({
+      host: 'felixmielcarek-bigbrotherdb',
+      user: process.env.MARIADB_USER,
+      database: process.env.MARIADB_DATABASE,
+      password: process.env.MARIADB_PASSWORD,
+      connectionLimit: 5
+    });
+
+    const conn = await pool.getConnection();
+    
+    conn.query(sqlQuery, [userId], (err, res) => {
+      if (err) {
+        console.error('Error executing query', err);
+        return;
+      }
+      console.log('Data deleted successfully');
       conn.end();
     });
   } catch (error) { console.log(`Error accessing database: ${error}`) }
