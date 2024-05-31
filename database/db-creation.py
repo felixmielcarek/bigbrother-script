4a38ce4e7b842438e101fc65e28893ec68b8e8c2
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2024-05-31 13:21:34 +0200

    Adding database creation with GitHub Actions

--- database/db-creation.py
+++ database/db-creation.py
@@ -1,35 +1,18 @@
-import os
-import mariadb
+import psycopg2
 
-env_user = os.getenv('MARIADB_USER')
-env_password = os.getenv('MARIADB_PASSWORD')
-env_database = os.getenv('MARIADB_DATABASE')
+conn = psycopg2.connect(
+   database="big-brother", user='postgres', password='postgres', host='127.0.0.1', port= '5432'
+)
+cursor = conn.cursor()
 
-def executeDBQuery(query):
-    conn = mariadb.connect(
-        user=env_user, 
-        password=env_password, 
-        host="felixmielcarek-bigbrotherdb",
-        database=env_database
-    )
-    conn.autocommit = True
-    cur = conn.cursor()
-    cur.execute(query)
-    cur.close()
-    conn.close()
+cursor.execute("DROP TABLE IF EXISTS USERS")
 
-grant_privileges_query = '''
-GRANT ALL PRIVILEGES ON {database}.* TO '{user}'@'%' IDENTIFIED BY '{password}';
-FLUSH PRIVILEGES;
-'''.format(database=env_database, user=env_user, password=env_password)
-
-create_table_query = '''
-CREATE TABLE IF NOT EXISTS users (
-    spotifyid VARCHAR(255) NOT NULL,
-    accesstoken VARCHAR(255),
-    refreshtoken VARCHAR(255),
-    PRIMARY KEY (spotifyid)
-);
-'''
-
-executeDBQuery(create_table_query)
+sql ='''CREATE TABLE USERS(
+   SPOTIFY_ID       VARCHAR(255) PRIMARY KEY,
+   ACCESS_TOKEN     VARCHAR(255),
+   REFRESH_TOKEN    VARCHAR(255)
+)'''
+cursor.execute(sql)
+print("Table created successfully........")
+conn.commit()
+conn.close()
